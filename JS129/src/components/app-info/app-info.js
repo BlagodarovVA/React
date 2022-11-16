93c19fffbd7a006d5f0037f7c82430534237101a
Valery <11w-walerik@mail.ru>
2022-11-16 17:07:33 +0300

    129 done

---
+++ JS129/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в ДИТ</h1>
+            <h2>Число сотрудников:</h2>
+            <h2>Премию получат:</h2>
+        </div>
+    )
+}
+
+export default AppInfo;

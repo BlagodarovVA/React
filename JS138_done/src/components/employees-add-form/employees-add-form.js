53032903848e9e047a9b8d7be12fb35e5a45bf8e
Valery <11w-walerik@mail.ru>
2022-11-23 16:33:11 +0300

    138 done

---
+++ JS138_done/src/components/employees-add-form/employees-add-form.js
@@ -0,0 +1,64 @@
+import {Component} from 'react';
+import './employees-add-form.css';
+
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
+
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        if (this.state.name.length > 3 && this.state.salary > 0) {
+            this.props.onAdd(this.state.name, this.state.salary);
+            this.setState({
+                name: '',
+                salary: ''
+            })
+        } else {
+            alert('Введите корректную информацию о сотруднике');
+        }
+    }    
+
+    render() {
+        const {name, salary} = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Добавить нового сотрудника</h3>
+                <form
+                    className="add-form d-flex"
+                    onSubmit = {this.onSubmit}>
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="ФИО?" 
+                        name="name"
+                        value={name}
+                        onChange={this.onValueChange}/>
+                    <input type="number"
+                        className="form-control new-post-label"
+                        placeholder="З/П в $" 
+                        name="salary"
+                        value={salary}
+                        onChange={this.onValueChange}/>
+    
+                    <button type="submit"
+                            className="btn btn-outline-light">Добавить</button>
+                    <button type="reset"
+                            className="btn btn-outline-light">Очистить</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default EmployeesAddForm;
